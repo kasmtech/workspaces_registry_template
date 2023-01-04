46c19fedca6cf8efbabe3b2baa00aab5302cbf77
Chris Hunt <chris.hunt@kasmweb.com>
2023-01-04 09:04:24 +0000

    Change App Store to App Registry

--- site/components/header.js
+++ site/components/header.js
@@ -63,7 +63,7 @@ export default function Header({ searchText, changeSearch }) {
 
       </div>
       <button className='p-4 relative z-10 px-5 bg-emerald-600 m-2 rounded items-center text-white/70 flex cursor-pointer' onClick={() => { copyToClipboard() }}>
-        <span className="mr-3">App Store Link</span>
+        <span className="mr-3">App Registry Link</span>
         <svg style={{ height: '14px', fill: '#fff' }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M224 0c-35.3 0-64 28.7-64 64V288c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H224zM64 160c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H288c35.3 0 64-28.7 64-64V384H288v64H64V224h64V160H64z" /></svg>
       </button>
     </header >
