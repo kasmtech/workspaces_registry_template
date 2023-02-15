856edf32f63e29c1bd2924f6bcda4a407f06b7c3
Chris Hunt <chris.hunt@kasmweb.com>
2023-02-15 10:27:09 +0000

    Remove unused dark declaration

--- site/components/Workspace.js
+++ site/components/Workspace.js
@@ -11,7 +11,7 @@ function Workspace({ Component, pageProps, workspace }) {
     }
     
     return (
-        <div onClick={() => viewexample(workspace)} className="w-[245px] h-[88px] transition-all relative cursor-pointer group flex p-2 items-center justify-center bg-slate-100/90 dark:bg-slate-900/90 shadow rounded hover:shadow-xl hover:bg-gradient-to-r hover:from-slate-900 hover:to-cyan-800 hover:text-white">
+        <div onClick={() => viewexample(workspace)} className="w-[245px] h-[88px] transition-all relative cursor-pointer group flex p-2 items-center justify-center bg-slate-100/90 shadow rounded hover:shadow-xl hover:bg-gradient-to-r hover:from-slate-900 hover:to-cyan-800 hover:text-white">
             <div className="w-full h-full">
                 <div className="show-grid flex h-full items-center">
                     <div className="kasmcard-img flex h-full mx-4 items-center justify-center">
